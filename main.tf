29b893b0f6049af4602303551f1865d4ac29673e
snehavb06 <146153718+snehavb06@users.noreply.github.com>
2025-03-12 22:37:18 +0530

    Update main.tf with keypair

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ provider "aws" {
 resource "aws_instance" "my_ec2" {
   ami           = "ami-0c2e61fdcb5495691"  # Replace with latest AMI ID for eu-north-1
   instance_type = "t2.micro"
-  key_name      = "zuplon"  # Replace with your AWS key pair
+  key_name      = "eu-key"  # Replace with your AWS key pair
 
   tags = {
     Name = "Terraform-GitHub-EC2"
