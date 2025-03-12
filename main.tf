e805db358280b3cf2ad8248499364c6656c87cb0
snehavb06 <146153718+snehavb06@users.noreply.github.com>
2025-03-12 22:23:15 +0530

    Update main.tf with ami

--- main.tf
+++ main.tf
@@ -1,11 +1,11 @@
 provider "aws" {
-  region = "ap-southeast-1"
+  region = "eu-north-1"
 }
 
 resource "aws_instance" "my_ec2" {
-  ami           = "ami-0672fd5b9210aa093"  # Replace with latest AMI ID for ap-southeast-1
+  ami           = "ami-0c2e61fdcb5495691"  # Replace with latest AMI ID for eu-north-1
   instance_type = "t2.micro"
-  key_name      = "new-sign"  # Replace with your AWS key pair
+  key_name      = "zuplon"  # Replace with your AWS key pair
 
   tags = {
     Name = "Terraform-GitHub-EC2"
