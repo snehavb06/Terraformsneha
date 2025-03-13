39c38c7bfabcf565726c164cd9576e282d57aca3
snehavb06 <146153718+snehavb06@users.noreply.github.com>
2025-03-13 21:57:47 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ provider "aws" {
 resource "aws_instance" "my_ec2" {
   ami           = "ami-09a9858973b288bdd"  # Use a valid, region-specific AMI ID
   instance_type = "t2.micro"               # Ensure the instance type is compatible with the AMI
-  key_name      = "New-ec2r"          # Specify an existing SSH key pair
+  key_name      = "New-ec2"          # Specify an existing SSH key pair
 
   # Security group (ensure it exists in your region)
   security_groups = ["default"]
